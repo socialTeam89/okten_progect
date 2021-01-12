2e7701099f3075f93b4cb60259ba3338e12b0ad9
Andre <demchyshyn95@gmail.com>
2021-01-12 20:59:55 +0200

    added files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import { Route,Switch,Redirect } from 'react-router';
+import SignIn from "./Components/SignIn/SignIn";
+import SignUp from "./Components/SignUp/SignUp";
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+        <Switch>
+          <Route path='/SingIn'  component={SignIn}/>
+          <Route path='/SingUp'  component={SignUp}/>
+          <Redirect to='/'/>
+        </Switch>
     </div>
   );
 }
